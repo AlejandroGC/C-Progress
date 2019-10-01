0ded31df33631f180efcb8e997060130e8a1adf7
Alex Gonzalez <alejandro.g.carrillo@hotmail.com>
2019-10-01 00:26:56 -0500

    operaciones aritmeticas

---
+++ variables.cpp
@@ -0,0 +1,21 @@
+//lectura o entrada de datos
+#include <iostream>
+
+using namespace std;
+
+/*
+Comentario muchas lineas
+*/
+
+int main(){
+    int number;
+
+    cout<<"Digite un numero: ";
+    cin>>number;
+
+    cout<<"\nEl numero que digito es: "<<number;
+
+
+
+    return 0;
+}
